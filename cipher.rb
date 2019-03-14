00efea26c686ba0bc033d37cfa3475a949fb7425
ifeanyi <aifeanyi019@gmail.com>
2019-03-14 16:40:13 +0100

    Initial commit

---
+++ cipher.rb
@@ -0,0 +1,24 @@
+#ASCII
+#a-z: 97-122
+#A-Z: 65-90
+def caesar_cipher(string, num)
+    newstring = ''
+    string.scan(/./) do |x|
+      asc = x.ord
+      if (97..122).include?(asc)
+        asc > 122 - num ? asc = 96 + (num - (122 - asc)) : asc += num
+      elsif (65..90).include?(asc)
+        asc > 90 - num ?  asc = 64 + (num - (90 - asc)) : asc += num
+      else
+        asc
+      end
+      new_character = asc.chr
+      newstring = newstring + new_character
+    end
+    newstring
+  end
+  
+  puts caesar_cipher("What a string!", 5)
+
+
+
